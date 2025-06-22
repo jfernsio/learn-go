27828476de034358a244e476b53a83d7e37fbdae
joel <vashappnin107@gmail.com>
2025-06-22 23:59:09 +0530

    methods added

--- struct/method.go
+++ struct/method.go
@@ -19,7 +19,19 @@ func (c Car) DisplayDetails() {
 //c Car) is the receiver that binds the method DisplayDetails to the Car struct.
 // The method can be called using the dot operator.
 
+//--> pointer reciver
+func (c *Car) UpdateName(newName string) {
+    c.Name = newName
+}	
 
+// Method to check operational status
+func (c Car) OperationalStatus() {
+    if c.Years > 15 {
+        fmt.Println(c.Name, "is no longer operational.")
+    } else {
+        fmt.Printf("%s has %d years of operation left.\n", c.Name, 15-c.Years)
+    }
+}
 func main() {
     // Initialize a Car instance
     car := Car{
@@ -33,5 +45,14 @@ func main() {
     fmt.Println("Car Name:", car.Name)
     fmt.Println("Years in Service:", car.Years)
 
+      // Call methods
+    car.DisplayDetails()
+    car.OperationalStatus()
+
+    // Update brand using pointer receiver
+    car.UpdateName("SpaceX")
+    fmt.Println("Updated Brand:", car.Brand)
 }
+
+
 
