d2020f6f638b20475b87d0c481aaf651448856b1
joel <vashappnin107@gmail.com>
2025-09-05 23:27:10 +0530

    smthg

--- bank-acc-man-sys/main.go
+++ bank-acc-man-sys/main.go
@@ -2,7 +2,9 @@ package main
 
 import ("fmt")
 
-
+type Bank struct {
+	Accounts []Account
+}
 
 type Account struct {
 	Id int
@@ -12,9 +14,15 @@ type Account struct {
 
 var accounts []Account
 
-func createAccount(id int, owner string, balance float64) Account {
+func printAccounts() {
+	for _,acc := range accounts {
+		fmt.Println(acc)
+	}
+}
+
+func createAccount(id int, owner string, balance float64) *Account {
 	accounts = append(accounts, Account{Id: id, Owner: owner, Balance: balance})
-	return accounts[len(accounts)-1]
+	return &accounts[len(accounts)-1]
 }
 
 func deposit(acc *Account, amount float64) error {
@@ -36,8 +44,24 @@ func withdraw(ac * Account, amount float64) error {
 	return nil
 }
 
+func findAccount(id int,accounts []Account) (*Account,error) {
+	if len(accounts) == 0 {
+		return nil, fmt.Errorf("no accounts available")
+	}
+  for i := 0; i < len(accounts); i++ {
+        if accounts[i].Id == id {
+            return &accounts[i], nil
+        }
+    }
+	return nil, fmt.Errorf("account not found")
+}
 
-
+func transfer(from, to *Account, amount float64) error {
+    if err := withdraw(from, amount); err != nil {
+        return err
+    }
+    return deposit(to, amount)
+}
 
 
 
@@ -48,16 +72,34 @@ func main () {
 	fmt.Println("Created Account:", acc)
 	acc2 := createAccount(2,"doe", 2000)
 	fmt.Println("Created Account:", acc2)
-	err := deposit(&acc,500)
+	err := deposit(acc,500)
 	if err != nil {
 		fmt.Println("Error during deposit:", err)
 	} else {
 		fmt.Println("After Deposit:", acc)
 	}
-	err = withdraw(&acc2, 2500)
+	err = withdraw(acc2, 2500)
 	if err != nil {
 		fmt.Println("Error during withdrawal:", err)
 	} else {
 		fmt.Println("After Withdrawal:", acc2)
 	}
+	foundAcc, err := findAccount(1, accounts)	
+	if err != nil {
+		fmt.Println("Error finding account:", err)
+	} else {
+		fmt.Println("Found Account:", *foundAcc)
+	}
+
+	err = transfer(acc,acc2,300)
+	if err != nil {
+		fmt.Println("Error during transfer:", err)
+	}else {
+		fmt.Println("After Transfer:")
+		fmt.Println("From Account:", acc)
+		fmt.Println("To Account:", acc2)
+	}
+
+	fmt.Println("All Accounts:")
+	printAccounts()
 }
