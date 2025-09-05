5c16012a49bc816cc1b6de9ebb90c2447d1a3aa1
joel <vashappnin107@gmail.com>
2025-09-06 00:05:16 +0530

    go routine on deposit func

--- bank-acc-man-sys/main.go
+++ bank-acc-man-sys/main.go
@@ -1,21 +1,24 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"sync"
+)
 
 type Bank struct {
 	Accounts []Account
 }
 
 type Account struct {
-	Id int
-	Owner string
+	Id      int
+	Owner   string
 	Balance float64
 }
 
 var accounts []Account
 
 func printAccounts() {
-	for _,acc := range accounts {
+	for _, acc := range accounts {
 		fmt.Println(acc)
 	}
 }
@@ -25,7 +28,12 @@ func createAccount(id int, owner string, balance float64) *Account {
 	return &accounts[len(accounts)-1]
 }
 
+var mu sync.Mutex
+
 func deposit(acc *Account, amount float64) error {
+	//lock to protect shared data
+	mu.Lock()
+	defer mu.Unlock()
 	if amount <= 0 {
 		return fmt.Errorf("deposit amount must be more than zero")
 	}
@@ -33,7 +41,7 @@ func deposit(acc *Account, amount float64) error {
 	return nil
 }
 
-func withdraw(ac * Account, amount float64) error {
+func withdraw(ac *Account, amount float64) error {
 	if amount <= 0 {
 		return fmt.Errorf("withdraw amount must be more than zero")
 	}
@@ -44,57 +52,65 @@ func withdraw(ac * Account, amount float64) error {
 	return nil
 }
 
-func findAccount(id int,accounts []Account) (*Account,error) {
+func findAccount(id int, accounts []Account) (*Account, error) {
 	if len(accounts) == 0 {
 		return nil, fmt.Errorf("no accounts available")
 	}
-  for i := 0; i < len(accounts); i++ {
-        if accounts[i].Id == id {
-            return &accounts[i], nil
-        }
-    }
+	for i := 0; i < len(accounts); i++ {
+		if accounts[i].Id == id {
+			return &accounts[i], nil
+		}
+	}
 	return nil, fmt.Errorf("account not found")
 }
 
 func transfer(from, to *Account, amount float64) error {
-    if err := withdraw(from, amount); err != nil {
-        return err
-    }
-    return deposit(to, amount)
+	if err := withdraw(from, amount); err != nil {
+		return err
+	}
+	return deposit(to, amount)
 }
 
-
-
-
-func main () {
+func main() {
 	fmt.Println("Bank Account Management System")
-	acc :=	createAccount(1,"faze", 1000)
+	acc := createAccount(1, "faze", 1000)
 	fmt.Println("Created Account:", acc)
-	acc2 := createAccount(2,"doe", 2000)
+	acc2 := createAccount(2, "doe", 2000)
 	fmt.Println("Created Account:", acc2)
-	err := deposit(acc,500)
-	if err != nil {
-		fmt.Println("Error during deposit:", err)
-	} else {
-		fmt.Println("After Deposit:", acc)
+
+	//wait for goroutines to finish
+	var wg sync.WaitGroup
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			defer wg.Done()
+			err := deposit(acc, 1)
+			if err != nil {
+				fmt.Println("Error during deposit:", err)
+			} else {
+				fmt.Println("After Deposit:", acc)
+			}
+		}()
 	}
-	err = withdraw(acc2, 2500)
+	wg.Wait()
+
+	err := withdraw(acc2, 2500)
 	if err != nil {
 		fmt.Println("Error during withdrawal:", err)
 	} else {
 		fmt.Println("After Withdrawal:", acc2)
 	}
-	foundAcc, err := findAccount(1, accounts)	
+	foundAcc, err := findAccount(1, accounts)
 	if err != nil {
 		fmt.Println("Error finding account:", err)
 	} else {
 		fmt.Println("Found Account:", *foundAcc)
 	}
 
-	err = transfer(acc,acc2,300)
+	err = transfer(acc, acc2, 1000)
 	if err != nil {
 		fmt.Println("Error during transfer:", err)
-	}else {
+	} else {
 		fmt.Println("After Transfer:")
 		fmt.Println("From Account:", acc)
 		fmt.Println("To Account:", acc2)
